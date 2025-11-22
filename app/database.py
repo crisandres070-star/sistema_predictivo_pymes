0d90dfc27717fe7a4066b2ae506711916a8fe578
Cristopher955 <cr.ormazabalp@duocuc.cl>
2025-11-22 14:07:22 -0300

    Fix get_db function

--- app/database.py
+++ app/database.py
@@ -1,12 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import Session
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+# ======================
+# ESTA FUNCIÓN FALTABA
+# ======================
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
